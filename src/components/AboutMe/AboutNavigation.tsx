f6b0a0405f4cde9152da2f54af17f4acdc56d0c8
JungJihun1012 <jihunjeong965@gmail.com>
2024-06-13 18:44:57 +0900

    :sparkles: Icon

--- src/components/AboutMe/AboutNavigation.tsx
+++ src/components/AboutMe/AboutNavigation.tsx
@@ -1,50 +1,58 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faCalendar, faEnvelope, faLocationDot, faPencil, faPhone, faUser} from "@fortawesome/free-solid-svg-icons";
-import { useLocation } from "react-router-dom";
-import styled from "styled-components"
-import { UserName } from "./UserName";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendar, faEnvelope, faLocationDot, faPencil, faPhone, faUser } from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+import { UserName } from "./UserName";
+import { BirthDay } from "./BirthDay";
+import { LoactionDot } from "./LocationDot";
+import { Phone } from "./Phone";
+import { Email } from "./Email";
+import { Pencil } from "./Pencil";
 
 export const AboutNavigation: React.FC = () => {
     const location = useLocation();
     return (
         <Container>
-            <Icon
-            icon={faUser}
-            $isSelected={location.pathname === "/"}
-            >
+            <IconWrapper $isSelected={location.pathname === "/"}>
+                <FontAwesomeIcon icon={faUser} />
                 <UserName />
-            </Icon>
-            <Icon
-            icon={faCalendar}
-            $isSelected={location.pathname.includes("/Calendar")}
-            />
-            <Icon
-            icon={faLocationDot}
-            $isSelected={location.pathname.includes("/LocationDot")}
-            />
-            <Icon
-            icon={faPhone}
-            $isSelected={location.pathname.includes("/Phone")}
-            />
-            <Icon
-            icon={faEnvelope}
-            $isSelected={location.pathname.includes("/Envelope")}
-            />
-            <Icon
-            icon={faPencil}
-            $isSelected={location.pathname.includes("/Pencil")}
-            />
+            </IconWrapper>
+            <IconWrapper $isSelected={location.pathname.includes("/Calendar")}>
+                <FontAwesomeIcon icon={faCalendar} />
+                <BirthDay />
+            </IconWrapper>
+            <IconWrapper $isSelected={location.pathname.includes("/LocationDot")}>
+                <FontAwesomeIcon icon={faLocationDot} />
+                <LoactionDot />
+            </IconWrapper>
+            <IconWrapper $isSelected={location.pathname.includes("/Phone")}>
+                <FontAwesomeIcon icon={faPhone} />
+                <Phone />
+            </IconWrapper>
+            <IconWrapper $isSelected={location.pathname.includes("/Envelope")}>
+                <FontAwesomeIcon icon={faEnvelope} />
+                <Email />
+            </IconWrapper>
+            <IconWrapper $isSelected={location.pathname.includes("/Pencil")}>
+                <FontAwesomeIcon icon={faPencil} />
+                <Pencil />
+            </IconWrapper>
         </Container>
-    )
-}
+    );
+};
 
-export const Container = styled.div`
+const Container = styled.div`
     display: flex;
+    flex-wrap: wrap;
     align-items: center;
     justify-content: center;
 `;
-export const Icon = styled(FontAwesomeIcon) <{ $isSelected?: boolean }>`
-   color: ${(props) => (props.$isSelected ? 'black' : '#BABABA')};
-   font-size: 24px;
+
+const IconWrapper = styled.div<{ $isSelected?: boolean }>`
+    display: flex;
+    align-items: center;
+    color: ${(props) => (props.$isSelected ? 'black' : 'black')};
+    font-size: 24px;
+    margin: 0 8px;
 `;
