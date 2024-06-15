2b45fc471ff714a98d4188502643e43bcf472e88
JungJihun1012 <jihunjeong965@gmail.com>
2024-06-15 12:50:40 +0900

    :sparkles: AboutMe 페이지 완성

--- src/components/AboutMe/AboutNavigation.tsx
+++ src/components/AboutMe/AboutNavigation.tsx
@@ -14,44 +14,62 @@ export const AboutNavigation: React.FC = () => {
     const location = useLocation();
     return (
         <Container>
-            <IconWrapper $isSelected={location.pathname === "/"}>
-                <FontAwesomeIcon icon={faUser} />
-                <UserName />
-            </IconWrapper>
-            <IconWrapper $isSelected={location.pathname.includes("/Calendar")}>
-                <FontAwesomeIcon icon={faCalendar} />
-                <BirthDay />
-            </IconWrapper>
-            <IconWrapper $isSelected={location.pathname.includes("/LocationDot")}>
-                <FontAwesomeIcon icon={faLocationDot} />
-                <LoactionDot />
-            </IconWrapper>
-            <IconWrapper $isSelected={location.pathname.includes("/Phone")}>
-                <FontAwesomeIcon icon={faPhone} />
-                <Phone />
-            </IconWrapper>
-            <IconWrapper $isSelected={location.pathname.includes("/Envelope")}>
-                <FontAwesomeIcon icon={faEnvelope} />
-                <Email />
-            </IconWrapper>
-            <IconWrapper $isSelected={location.pathname.includes("/Pencil")}>
-                <FontAwesomeIcon icon={faPencil} />
-                <Pencil />
-            </IconWrapper>
+            <Content>
+                <IconWrapper $isSelected={location.pathname === "/"}>
+                    <FontAwesomeIcon icon={faUser} />
+                    <UserName />
+                </IconWrapper>
+                <IconWrapper $isSelected={location.pathname.includes("/Calendar")}>
+                    <FontAwesomeIcon icon={faCalendar} />
+                    <BirthDay />
+                </IconWrapper>
+                <IconWrapper $isSelected={location.pathname.includes("/LocationDot")}>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    <LoactionDot />
+                </IconWrapper>
+            </Content>
+            <IconContent>
+
+                <IconWrapper $isSelected={location.pathname.includes("/Phone")}>
+                    <FontAwesomeIcon icon={faPhone} />
+                    <Phone />
+                </IconWrapper>
+                <IconWrapper $isSelected={location.pathname.includes("/Envelope")}>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <Email />
+                </IconWrapper>
+                <IconWrapper $isSelected={location.pathname.includes("/Pencil")}>
+                    <FontAwesomeIcon icon={faPencil} />
+                    <Pencil />
+                </IconWrapper>
+            </IconContent>
         </Container>
     );
 };
 
 const Container = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
+    display: block;
+    padding-top: 50px;
 `;
 
+const Content = styled.div`
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    line-height: 2.5em;
+    gap: 17%;
+`;
+const IconContent = styled.div`
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    line-height: 2.5em;
+    gap: 10%;
+`;
 const IconWrapper = styled.div<{ $isSelected?: boolean }>`
     display: flex;
     align-items: center;
+    gap: 40px;
     color: ${(props) => (props.$isSelected ? 'black' : 'black')};
     font-size: 24px;
     margin: 0 8px;
