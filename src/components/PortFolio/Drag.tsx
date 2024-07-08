e741850d9aa05a35f92129b975ba83fa520f12c8
JungJihun1012 <jihunjeong965@gmail.com>
2024-07-08 10:26:29 +0900

    :sparkles: Swiper 모듈 오류..

--- src/components/PortFolio/Drag.tsx
+++ src/components/PortFolio/Drag.tsx
@@ -1,26 +1,48 @@
-import styled from "styled-components"
-import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.css';
 
-export const Drag = () => {
-    return(
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={3}
-                scrollbar={{draggable: true}}
-                navigation
-                pagination={{clickable: true}}
-                breakpoints={{
-                    768: {
-                        slidesPerView: 7,
-                    },
-                }}
-            >
-                <SwiperSlide>slide 1</SwiperSlide>
-                <SwiperSlide>slide 2</SwiperSlide>
-                <SwiperSlide>slide 3</SwiperSlide>
-                <SwiperSlide>slide 4</SwiperSlide>
-            </Swiper>
-    )
+import { ReactElement, useEffect, useRef, useState } from "react"
+import SwiperCore, { Navigation, Scrollbar } from 'swiper';
+import {Swiper, SwiperSlide} from 'swiper/react';
+
+interface MainSliderProps {
+    cardList: ReactElement[];
+    slidesPerView: 3 | 4;
 }
 
-export const Container = styled.div``;
+export const Drag = (props: MainSliderProps) => {
+    const {cardList, slidesPerView } = props;
+
+    SwiperCore.use([Navigation, Scrollbar]);
+
+    const prevRef = useRef<HTMLButtonElement>(null);
+    const nextRef = useRef<HTMLButtonElement>(null);
+    const [swiperSetting, setSwiperSetting] = useState<Swiper | null>(null);    
+
+    useEffect(() => {
+        if(!swiperSetting) {
+            setSwiperSetting({
+                spaceBetween: 24,
+                navigation: {
+                    prevEl: prevRef.current,
+                    nextEl: nextRef.current,
+                },
+                scrollbar: {draggable: true, el: null},
+                slidesPerView,
+                onBeforeInit: (swiper: SwiperCore) => {
+                    if(typeof swiper.params.navigation !== 'boolean') {
+                        if(swiper.params.navigation) {
+                            swiper.params.navigation.prevEl = prevRef.current;
+                            swiper.params.navigation.nextEl = nextRef.current;
+                        }
+                    }
+                    swiper.navigation.update();
+                },
+            });
+        }
+    }, [swiperSetting, setSwiperSetting]);
+    return(
+        <>
+
+        </>
+    )
+}
