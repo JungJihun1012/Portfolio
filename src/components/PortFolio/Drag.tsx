e43a02dceb19dcb35f988b87485c15bf7e485367
JungJihun1012 <jihunjeong965@gmail.com>
2024-07-13 19:06:58 +0900

    :sparkles: Drag 수정

--- src/components/PortFolio/Drag.tsx
+++ src/components/PortFolio/Drag.tsx
@@ -1,19 +1,19 @@
-// import styled from "styled-components";
-// import { AnimatePresence, motion, wrap } from 'framer-motion';
-// import { useState } from "react";
+import styled from "styled-components";
+import { AnimatePresence, motion, wrap } from 'framer-motion';
+import { useState } from "react";
 
-// export const Drag = () => {
-//     const [visable, setVisable] = useState(0);
+export const Drag = () => {
+    const [visable, setVisable] = useState(0);
 
-//     const imageIndex = wrap(0, images.length, visable);
-//     return(
-//         <Container>
-//             <AnimatePresence custom={back}>
+    const imageIndex = wrap(0, images.length, visable);
+    return(
+        <Container>
+            <AnimatePresence custom={back}>
 
-//             </AnimatePresence>
-//         </Container>
-//     ); 
-// }
+            </AnimatePresence>
+        </Container>
+    ); 
+}
 
-// export const Container = styled.div``;
-// export const Box = styled(motion.div)``;
+export const Container = styled.div``;
+export const Box = styled(motion.div)``;
