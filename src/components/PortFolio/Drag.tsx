110e03052f1900018b399203b6d44d8ef6e10655
JungJihun1012 <jihunjeong965@gmail.com>
2024-07-15 19:43:03 +0900

    test

--- src/components/PortFolio/Drag.tsx
+++ src/components/PortFolio/Drag.tsx
@@ -1,19 +1,11 @@
-import styled from "styled-components";
-import { AnimatePresence, motion, wrap } from 'framer-motion';
-import { useState } from "react";
+import styled from "styled-components"
 
 export const Drag = () => {
-    const [visable, setVisable] = useState(0);
-
-    const imageIndex = wrap(0, images.length, visable);
     return(
         <Container>
-            <AnimatePresence custom={back}>
-
-            </AnimatePresence>
+            
         </Container>
-    ); 
+    )
 }
 
 export const Container = styled.div``;
-export const Box = styled(motion.div)``;
