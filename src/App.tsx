2742d19a272f60891cdad8cac103cedd57ed0145
JungJihun1012 <jihunjeong965@gmail.com>
2024-08-16 19:16:31 +0900

    :sparkles: 백엔드 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtfolioPage from "./pages/ProtfolioPage";
+import { useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
+  const getData = async () => {
+    const user = await axios.get("http://localhost:3000/")
+
+    console.log(user.data);
+    
+  }
+  useEffect(() => {
+    getData();
+  }, []);
   return(
     <BrowserRouter>
       <Routes>
