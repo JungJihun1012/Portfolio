997e62aa72973ac69eb90de117c6db0c285187cc
JungJihun1012 <134596552+JungJihun1012@users.noreply.github.com>
2024-12-13 17:02:49 +0900

    Update Drag.tsx

--- src/components/PortFolio/Drag.tsx
+++ src/components/PortFolio/Drag.tsx
@@ -57,7 +57,7 @@ const Drag: React.FC = () => {
                     drag="x"
                     dragConstraints={{ left: 0, right: 0 }}
                     dragElastic={1}
-                    onDragEnd={(e, { offset, velocity }) => {
+                    onDragEnd={({ offset, velocity }) => {
                         const swipe = swipePower(offset.x, velocity.x);
 
                         if (swipe < - swipeConfidenceThreshold) {
